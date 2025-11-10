2dcdeac64ac1a226da952eccfbf03c2c1c1ef857
Jim <yunjin.li0817@gmail.com>
2025-11-10 17:13:54 +0100

    fix(dataset): Remove srun from the bash script

--- data/NeRF-DS/download_dataset.bash
+++ data/NeRF-DS/download_dataset.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/JokerYan/NeRF-DS/releases/download/v0.1-pre-release/NeRF-DS.dataset.zip
-srun python -m zipfile -e NeRF-DS.dataset.zip .
+python -m zipfile -e NeRF-DS.dataset.zip .
 rm NeRF-DS.dataset.zip
